2c3ab1365752f5d91440edb24ad88e3ae1760b4a
Plamadeala Max <linkolnmax24@gmail.com>
2024-05-08 20:45:51 +0300

    feat: add periodic extraction (not for high load)

--- main.go
+++ main.go
@@ -4,16 +4,41 @@ import (
 	"fmt"
 	"github.com/grumpycatyo-collab/bug-free-goggles/core"
 	"github.com/grumpycatyo-collab/bug-free-goggles/models"
+	"time"
 )
+
+func main() {
+
+	intervals := map[string]time.Duration{
+		"every 5 minutes":  time.Minute * 5,
+		"every 30 seconds": time.Second * 30,
+	}
+
+	projectsChannel := make(chan []models.Project)
+	usersChannel := make(chan []models.User)
+	for intervalName, interval := range intervals {
+		go extract(interval, projectsChannel, usersChannel)
+		fmt.Printf("Started fetching data %s\n", intervalName)
+	}
+
+	for {
+		projectsData := <-projectsChannel
+		fmt.Println(projectsData)
+		usersData := <-usersChannel
+		fmt.Println(usersData)
+	}
+}
 
 var projects []models.Project
 var users []models.User
 
-func main() {
-	projects, _ = core.GetProjects()
-	fmt.Printf("%v", projects)
-
-	users, _ = core.GetUsers()
-	fmt.Printf("\n%v", users)
+func extract(interval time.Duration, chP chan<- []models.Project, chU chan<- []models.User) {
+	for {
+		projects, _ = core.GetProjects()
+		users, _ = core.GetUsers()
+		chP <- projects
+		chU <- users
+		time.Sleep(interval)
+	}
 
 }
